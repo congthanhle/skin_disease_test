201bc89c3910d410463c53003e60570014d55f88
congthanhle <lecongthanhbaoloclamdong@gmail.com>
2024-06-15 12:46:05 +0700

    update link

--- app/routes.py
+++ app/routes.py
@@ -21,7 +21,7 @@ celery.conf.update(app.config)
 
 
 SKIN_CLASSES = {
-  0: 'Actinic Keratoses (Solar Keratoses) or intraepithelial Carcinoma (Bowen’s disease)', #Viêm giác mạc
+  0: 'Actinic keratosis', #Viêm giác mạc
   1: 'Basal Cell Carcinoma', # Ung thư gia
   2: 'Benign Keratosis',  #Viêm giac mạc bã nhờn
   3: 'Dermatofibroma', # Viêm gia cơ địa
@@ -29,6 +29,17 @@ SKIN_CLASSES = {
   5: 'Melanocytic Nevi',
   6: 'Vascular skin lesion' # Tổn thương mạch máu ở da
 }
+
+SKIN_CLASSES_LINK = {
+  0: 'https://dermnetnz.org/topics/actinic-keratosis', #Viêm giác mạc
+  1: 'https://dermnetnz.org/topics/basal-cell-carcinoma', # Ung thư gia
+  2: 'https://dermnetnz.org/topics/seborrhoeic-keratosis',  #Viêm giac mạc bã nhờn
+  3: 'https://dermnetnz.org/topics/dermatofibroma', # Viêm gia cơ địa
+  4: 'https://dermnetnz.org/topics/melanoma',      # Khối u ác tính
+  5: 'https://dermnetnz.org/topics/melanocytic-naevus',
+  6: 'https://www.ssmhealth.com/cardinal-glennon/services/pediatric-plastic-reconstructive-surgery/hemangiomas#:~:text=Vascular%20lesions%20are%20relatively%20common,Vascular%20Malformations%2C%20and%20Pyogenic%20Granulomas.' # Tổn thương mạch máu ở da
+}
+
 def generate_chart(prediction_probs):
     plt.figure(figsize=[50,50])
     plt.pie(prediction_probs)
@@ -65,8 +76,10 @@ def upload_file():
         pred = np.argmax(prediction)
         disease = SKIN_CLASSES[pred]
         accuracy = prediction[0][pred]
+        link = SKIN_CLASSES_LINK[pred]
         generate_chart(prediction[0]) 
-    return render_template('predict.html', title='Success', predictions=disease, acc=accuracy*100, img_file=f.filename)
+
+    return render_template('predict.html', title='Success', skinDiseaseLink=link, predictions=disease, acc=accuracy*100, img_file=f.filename)
 
 @app.route('/health')
 def health():
